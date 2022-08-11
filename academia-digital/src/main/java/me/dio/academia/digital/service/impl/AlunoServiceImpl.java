ab74ab36a5bb5a230257d97f81079ec1df807da3
regissanme <rsanme@gmail.com>
2022-08-11 00:35:01 -0300

    AlunoService implementation

---
+++ academia-digital/src/main/java/me/dio/academia/digital/service/impl/AlunoServiceImpl.java
@@ -0,0 +1,56 @@
+package me.dio.academia.digital.service.impl;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.entity.form.AlunoUpdateForm;
+import me.dio.academia.digital.repository.AlunoRepository;
+import me.dio.academia.digital.service.IAlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Projeto: academia-digital
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 11/08/2022
+ * Hora: 00:01
+ */
+@Service
+public class AlunoServiceImpl implements IAlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+
+    @Override
+    public Aluno create(AlunoForm form) {
+        Aluno aluno = new Aluno();
+        aluno.setNome(form.getNome());
+        aluno.setCpf(form.getCpf());
+        aluno.setBairro(form.getBairro());
+        aluno.setDataDeNascimento(form.getDataDeNascimento());
+
+        return alunoRepository.save(aluno);
+    }
+
+    @Override
+    public Aluno get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Aluno> getAll() {
+        return alunoRepository.findAll();
+    }
+
+    @Override
+    public Aluno update(Long id, AlunoUpdateForm formUpdate) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
