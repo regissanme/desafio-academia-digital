b3f3906a0b202df21e37256bea28a01086825967
regissanme <rsanme@gmail.com>
2022-08-11 00:36:12 -0300

    update AlunoController e AlunoForm

--- academia-digital/src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ academia-digital/src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -1,9 +1,33 @@
 package me.dio.academia.digital.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.service.impl.AlunoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
+
+    @Autowired
+    private AlunoServiceImpl service;
+
+    @GetMapping
+    public ResponseEntity<List<Aluno>> getAll(){
+        return ResponseEntity.ok().body(service.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Aluno> create(@RequestBody AlunoForm alunoForm) throws URISyntaxException {
+        Aluno alunoSalvo = service.create(alunoForm);
+        URI uri = new URI("/alunos/" + alunoSalvo.getId());
+
+        return ResponseEntity.created(uri).body(alunoSalvo);
+    }
 }
